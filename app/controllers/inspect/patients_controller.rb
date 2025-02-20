7f99bbacf9a8d7c08ca37d97198be19cef796666
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-02-20 09:28:09 +0000

    Add default graph options on first page load

--- app/controllers/inspect/patients_controller.rb
+++ app/controllers/inspect/patients_controller.rb
@@ -3,11 +3,20 @@
 module Inspect
   class PatientsController < ApplicationController
     def show
+      # Merge default values into the query parameters if they aren’t present.
+      defaults = {
+        include_consents: '1',
+        include_class_imports: '0',
+        other_patient_ids: '',
+        other_parent_ids: ''
+      }
+      params.reverse_merge!(defaults)
+
       @patient = policy_scope(Patient).find(params[:patient_id])
 
       # Read the filter parameter from params
-      @include_consents = params[:include_consents] == '1'
-      @include_class_imports = params[:include_class_imports] == '1'
+      @include_consents = params[:include_consents].last == '1'
+      @include_class_imports = params[:include_class_imports].last == '1'
       @other_patient_ids = params[:other_patient_ids].split(',').map { |s| s.strip.to_i }
       @other_parent_ids = params[:other_parent_ids].split(',').map { |s| s.strip.to_i }
 
