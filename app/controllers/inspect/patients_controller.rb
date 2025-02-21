98ec5ac1a84668604333e2bf579b368960e5e694
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-02-21 14:31:45 +0000

    Lint

--- app/controllers/inspect/patients_controller.rb
+++ app/controllers/inspect/patients_controller.rb
@@ -5,34 +5,51 @@ module Inspect
     def show
       # Merge default values into the query parameters if they aren’t present.
       defaults = {
-        include_consents: '1',
-        include_class_imports: '0',
-        other_patient_ids: '',
-        other_parent_ids: ''
+        include_consents: "1",
+        include_class_imports: "0",
+        other_patient_ids: "",
+        other_parent_ids: ""
       }
       params.reverse_merge!(defaults)
 
       @patient = policy_scope(Patient).find(params[:patient_id])
 
       # Read the filter parameter from params
-      @include_consents = params[:include_consents].last == '1'
-      @include_class_imports = params[:include_class_imports].last == '1'
-      @other_patient_ids = params[:other_patient_ids].split(',').map { |s| s.strip.to_i }
-      @other_parent_ids = params[:other_parent_ids].split(',').map { |s| s.strip.to_i }
+      @include_consents = params[:include_consents].last == "1"
+      @include_class_imports = params[:include_class_imports].last == "1"
+      @other_patient_ids =
+        params[:other_patient_ids].split(",").map { |s| s.strip.to_i }
+      @other_parent_ids =
+        params[:other_parent_ids].split(",").map { |s| s.strip.to_i }
 
       # Generate graph
-      @mermaid = GraphRecords.new(params[:patient_id],
-                                  focus_config: {
-                                    patient: [params[:patient_id]] + @other_patient_ids,
-                                    parents: @other_parent_ids
-                                  },
-                                  traversals_config: {
-                                    patient: %i[parents consents class_imports cohort_imports],
-                                    parent: %i[consents class_imports cohort_imports],
-                                  },
-                                  node_order: %i[class_import cohort_import patient consent parent],
-      ).graph(patient: [params[:patient_id]] + @other_patient_ids,
-              parent: @other_parent_ids).join("\n")
+      @mermaid =
+        GraphRecords
+          .new(
+            traversals_config: {
+              patient: %i[
+                sessions
+                parents
+                consents
+                class_imports
+                cohort_imports
+              ],
+              parent: %i[consents class_imports cohort_imports],
+            },
+            node_order: %i[
+              session
+              class_import
+              cohort_import
+              patient
+              consent
+              parent
+            ]
+          )
+          .graph(
+            patient: [@patient.id] + @other_patient_ids,
+            parent: @other_parent_ids
+          )
+          .join("\n")
     end
   end
 end
