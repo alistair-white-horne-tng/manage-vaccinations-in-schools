56057d7ff57aabb495217ba31ac7752432b9ad19
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-02-21 11:16:22 +0000

    Generate graph using GraphRecords

--- app/controllers/inspect/patients_controller.rb
+++ app/controllers/inspect/patients_controller.rb
@@ -20,33 +20,19 @@ module Inspect
       @other_patient_ids = params[:other_patient_ids].split(',').map { |s| s.strip.to_i }
       @other_parent_ids = params[:other_parent_ids].split(',').map { |s| s.strip.to_i }
 
-      # Pass the option to your graph builder. Adjust the API as needed.
-      @mermaid = "flowchart TB
-  classDef patient fill:#c2e598,color:#000
-  classDef parent fill:#faa0a0,color:#000
-  classDef consent fill:#fffaa0,color:#000
-  classDef class_import fill:#7fd7df,color:#000
-  classDef patient_highlighted fill:#c2e598,stroke:#000,stroke-width:3px,color:#000
-  classDef parent_highlighted fill:#faa0a0,stroke:#000,stroke-width:3px,color:#000
-  patient-16390:::patient_highlighted
-  patient-16390:::patient_highlighted --> parent-42558:::parent
-  parent-42558:::parent
-  consent-79740:::consent --> parent-42558:::parent
-  class_import-3367:::class_import --> parent-42558:::parent
-  patient-16390:::patient_highlighted --> parent-42557:::parent
-  parent-42557:::parent
-  consent-79751:::consent --> parent-42557:::parent
-  class_import-3367:::class_import --> parent-42557:::parent
-  class_import-3862:::class_import --> parent-42557:::parent
-  patient-16390:::patient_highlighted --> consent-79740:::consent
-  patient-16390:::patient_highlighted --> consent-79751:::consent
-  class_import-3367:::class_import --> patient-16390:::patient_highlighted
-  class_import-3862:::class_import --> patient-16390:::patient_highlighted"
-      # @mermaid = policy_scope(GraphPatient).call(params[:patient_id],
-      #                                            @other_patient_ids,
-      #                                            @other_parent_ids,
-      #                                            @include_consents,
-      #                                            @include_class_imports)
+      # Generate graph
+      @mermaid = GraphRecords.new(params[:patient_id],
+                                  focus_config: {
+                                    patient: [params[:patient_id]] + @other_patient_ids,
+                                    parents: @other_parent_ids
+                                  },
+                                  traversals_config: {
+                                    patient: %i[parents consents class_imports cohort_imports],
+                                    parent: %i[consents class_imports cohort_imports],
+                                  },
+                                  node_order: %i[class_import cohort_import patient consent parent],
+      ).graph(patient: [params[:patient_id]] + @other_patient_ids,
+              parent: @other_parent_ids).join("\n")
     end
   end
 end
