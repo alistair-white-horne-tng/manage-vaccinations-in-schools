49129f2738d1da55632650bf0aebb4d042853ed2
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-02-21 15:08:08 +0000

    Add test for node limit

--- spec/lib/graph_records_spec.rb
+++ spec/lib/graph_records_spec.rb
@@ -2,13 +2,16 @@
 
 describe GraphRecords do
   subject(:graph) do
-    described_class.new(
-      traversals_config: {
-        patient: %i[parents consents class_imports cohort_imports],
-        parent: %i[consents class_imports cohort_imports],
-        consent: %i[patient]
-      }
-    ).graph(patients: [patient])
+    described_class.new(traversals_config: traversals_config)
+                   .graph(patients: [patient])
+  end
+
+  let(:traversals_config) do
+    {
+      patient: %i[parents consents class_imports cohort_imports],
+      parent: %i[consents class_imports cohort_imports],
+      consent: %i[patient]
+    }
   end
 
   let!(:programmes) { [create(:programme, :hpv)] }
@@ -68,5 +71,22 @@ describe GraphRecords do
       "  class_import-#{class_import.id} --> patient-#{patient.id}",
       "  cohort_import-#{cohort_import.id} --> patient-#{patient.id}"
     )
+  end
+
+  context "when node limit is exceeded" do
+    subject(:graph_exceeded) do
+      described_class.new(
+        traversals_config: traversals_config,
+        node_limit: 1  # A very low limit to trigger recursion limit early
+      ).graph(patients: [patient])
+    end
+
+    it "returns a fallback Mermaid diagram with the error message in a red box" do
+      error_message = "Recursion limit of 1 nodes has been exceeded. Try restricting the graph."
+      expect(graph_exceeded).to include("flowchart TB")
+      # Assuming the error node is named `error` we check its content.
+      expect(graph_exceeded.join).to include("error[#{error_message}]")
+      expect(graph_exceeded.join).to include("style error fill:#f88,stroke:#f00,stroke-width:2px")
+    end
   end
 end
