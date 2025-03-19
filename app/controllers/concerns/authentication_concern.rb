1279d8efa20a79da32b7a3ae594e0a8ee6c53683
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-03-19 11:47:52 +0000

    Add `authenticate_basic_always` to always require authentication at certain endpoints
    - Implemented for use in the new ops endpoints:
      - `inspect/graph/{object_type}/{id}`
      - `inspect/timeline/{patient_id}`

--- app/controllers/concerns/authentication_concern.rb
+++ app/controllers/concerns/authentication_concern.rb
@@ -63,18 +63,24 @@ module AuthenticationConcern
     end
 
     def authenticate_basic
-      if Flipper.enabled? :basic_auth
-        authenticated =
-          authenticate_with_http_basic do |username, password|
-            username == Rails.application.credentials.support_username &&
-              password == Rails.application.credentials.support_password
-          end
+      perform_basic_auth if Flipper.enabled? :basic_auth
+    end
 
-        unless authenticated
-          request_http_basic_authentication "Application", <<~MESSAGE
+    def authenticate_basic_always
+      perform_basic_auth
+    end
+
+    def perform_basic_auth
+      authenticated =
+        authenticate_with_http_basic do |username, password|
+          username == Rails.application.credentials.support_username &&
+            password == Rails.application.credentials.support_password
+        end
+
+      unless authenticated
+        request_http_basic_authentication "Application", <<~MESSAGE
         Access is currently restricted to authorised users only.
       MESSAGE
-        end
       end
     end
 
