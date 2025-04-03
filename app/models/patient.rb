b57c13a9b0c8ff5afba4705e70eca79229a21f5d
Alistair White-Horne <alistair.whitehorne@tngtech.com>
2025-04-03 14:14:30 +0100

    Move normalisation function into the `String` class

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -417,12 +417,4 @@ class Patient < ApplicationRecord
       .where(session: sessions_for_current_academic_year)
       .find_each(&:destroy_if_safe!)
   end
-
-  def normalize_name(name)
-    name.gsub(/\s+/, " ")
-  end
-
-  def normalize_nhs_number(nhs_number)
-    nhs_number.gsub(/\s+/, " ")
-  end
 end
